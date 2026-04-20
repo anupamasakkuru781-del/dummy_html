42303aea0c863a525c0cc838839cdf2969d148ac
anupamasakkuru781-del <anupamasakkuru781@gmail.com>
2026-04-20 18:47:33 +0530

    Add files via upload
    html,css

---
+++ day wise class/day10.js
@@ -0,0 +1,43 @@
+// wt is js?
+// js is a script program languages
+// is an contact side programming language
+// is an server side programming language
+// is an full stack programming lanuage
+// it is used to create a dymanic website
+
+
+// ;- used to end the statement
+// ,- comma used to separate the values
+// {}- used to create a blocks
+// ()- parameters used to recive the values
+// []- used to create an array
+
+
+// datatypes- 
+// string- " ", ''
+// number- ex: 1,2,3,  1.1,1.2,1.3
+// boolean - true, or false
+//  null- null is also a value
+// undefined- defined is also a value(not defined any value)
+// array- array is a collection of values 
+// object- object is a collection of key value pairs
+
+
+// used to print the values in the counsle it is for debugging, development
+//  consule.warn - used to print the warning in the consule
+// consule.error- used to print the error in the consule
+
+// var name;  declartion
+// name = subhuam; assigment
+
+var name="subham"; // declaration and asigment to greater is call intitalization
+
+// name = "puneeths";
+
+// CSSCounterStyleRule.log(object);   
+name = 10; // reassigment / reinititalization
+
+
+// name = "puneeths";
+// console.log(name);
+
